c2c58cb8fd22687e79909223777dc5b6c6fad764
Hu6r1s <darmaa333@gmail.com>
2025-06-12 16:24:51 +0900

    init commit

---
+++ src/test/java/com/hu6r1s/bloom/BloomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hu6r1s.bloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

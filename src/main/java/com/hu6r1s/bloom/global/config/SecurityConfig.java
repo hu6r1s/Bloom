9b223dad1db682cd378122de58b456b9d941fdec
Hu6r1s <darmaa333@gmail.com>
2025-06-29 19:08:26 +0900

    fix(config): form login disable

--- src/main/java/com/hu6r1s/bloom/global/config/SecurityConfig.java
+++ src/main/java/com/hu6r1s/bloom/global/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -25,7 +26,7 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
-        .csrf(csrf -> csrf.disable())
+        .csrf(AbstractHttpConfigurer::disable).formLogin(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(authorization -> authorization
             .requestMatchers("/chat-test.html", "/api/v1/login").permitAll()
             .anyRequest().authenticated()
